8ec20334cbd3f3e676667d053ced90d119ece418
Adem ilter <ademilter@gmail.com>
2020-06-06 00:44:05 +0300

    body-text komponenti oluşturuldu

---
+++ components/text-body.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import cn from 'classnames'
+
+import styles from './text-body.module.css'
+
+function TextBody({ bold = false, className, children, ...props }) {
+  return (
+    <span
+      className={cn([styles.body, bold && styles.bold, className])}
+      {...props}
+    >
+      {children}
+    </span>
+  )
+}
+export default TextBody
