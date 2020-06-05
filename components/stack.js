702903bd9e8eecfd5a8a31ff7795edf75cb74369
Adem ilter <ademilter@gmail.com>
2020-06-06 00:14:19 +0300

    stack komponenti oluşturuldu

---
+++ components/stack.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import cn from 'classnames'
+
+import styles from './stack.module.css'
+
+function Stack({ gap = 10, column = false, children }) {
+  return (
+    <div
+      className={cn([styles.stack, column && styles.column])}
+      style={{ '--gap': `${gap}px` }}
+    >
+      {children}
+    </div>
+  )
+}
+export default Stack
