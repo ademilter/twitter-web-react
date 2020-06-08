b95ef1078707a0d8ae578444c26170bdf7aa3c43
Adem ilter <ademilter@gmail.com>
2020-06-08 21:25:05 +0300

    photo komponentine size özelliği eklendi

--- components/photo.js
+++ components/photo.js
@@ -5,10 +5,11 @@ import styles from './photo.module.css'
 
 function Photo({
   src = 'https://pbs.twimg.com/profile_images/1257786508871979009/O7g7fRVQ_400x400.jpg',
-  alt
+  alt,
+  size = 47
 }) {
   return (
-    <div className={cn([styles.photo])}>
+    <div className={cn([styles.photo])} style={{ width: size, height: size }}>
       <img className={styles.img} src={src} alt={alt} />
     </div>
   )
