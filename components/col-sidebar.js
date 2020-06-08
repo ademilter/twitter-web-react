149053e11c1c42bee0d0ab1937538cd59c738785
Adem ilter <ademilter@gmail.com>
2020-06-08 21:25:38 +0300

    responsive için layout ayarlandı

--- components/col-sidebar.js
+++ components/col-sidebar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import cn from 'classnames'
 
 import styles from './col-sidebar.module.css'
+
 import Navigation from './navigation'
 import ThemeButton from './theme-button'
 import ProfileBox from './profile-box'
@@ -10,10 +11,16 @@ function Layout({ flat }) {
   return (
     <div className={cn(styles.sidebar)}>
       <Navigation flat={flat} />
-      <ThemeButton big full>
-        Tweet
-      </ThemeButton>
-      <ProfileBox />
+
+      <div className={styles.tweet}>
+        <ThemeButton big full={!flat}>
+          {flat ? 'a' : 'Tweet'}
+        </ThemeButton>
+      </div>
+
+      <div className={styles.profile}>
+        <ProfileBox flat={flat} />
+      </div>
     </div>
   )
 }
