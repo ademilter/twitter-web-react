f84e89560d1cffabc5f8f802c0285cf8cce4f80f
Adem ilter <ademilter@gmail.com>
2020-06-22 00:53:20 +0300

    button componenti için link veya button olarak seçenek eklendi

--- components/button.js
+++ components/button.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import cn from 'classnames'
+import Link from 'next/link'
 
 import styles from './button.module.css'
 
-function Button({ full = false, children, className, ...props }) {
+function LinkButton({ href, children, ...props }) {
   return (
-    <button
-      type="button"
+    <Link href={href}>
+      <a {...props}>{children}</a>
+    </Link>
+  )
+}
+
+function BaseButton({ children, ...props }) {
+  return (
+    <button type="button" {...props}>
+      {children}
+    </button>
+  )
+}
+
+function Button({ full = false, children, className, ...props }) {
+  const Comp = props.href ? LinkButton : BaseButton
+
+  return (
+    <Comp
       className={cn(styles.button, full && styles.fullWidth, className)}
       {...props}
     >
       {children}
-    </button>
+    </Comp>
   )
 }
 
