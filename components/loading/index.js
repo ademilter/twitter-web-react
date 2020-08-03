5aceca0f4b3a1aea4e1a68d5d98e697c8ee6330c
Adem ilter <ademilter@gmail.com>
2020-08-03 13:29:14 +0300

    ana sayfadaki loading ortalandı

---
+++ components/loading/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Loading({ size = 30, stroke = 2 }) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 38 38"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="rgba(29, 161, 242, 1)"
+    >
+      <g fill="none" fillRule="evenodd">
+        <g transform="translate(1 1)" strokeWidth={stroke}>
+          <circle strokeOpacity=".3" cx="18" cy="18" r="18" />
+          <path d="M36 18c0-9.94-8.06-18-18-18">
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              from="0 18 18"
+              to="360 18 18"
+              dur="1s"
+              repeatCount="indefinite"
+            />
+          </path>
+        </g>
+      </g>
+    </svg>
+  )
+}
+export default Loading
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import useSWR from 'swr'
 
+import styles from './index.module.css'
+
 import Layout from '../components/layout'
 import Tweet from '../components/tweet'
 import fetcher from '../lib/fetch'
+import Loading from '../components/loading'
 
 function HomePage() {
   const { data } = useSWR('/api/tweet', fetcher)
 
   return (
     <Layout>
-      {!data && <p>Loading</p>}
+      {!data && (
+        <div className={styles.loading}>
+          <Loading />
+        </div>
+      )}
 
       {data?.statuses.map((tweet) => {
         return <Tweet key={tweet.id} {...tweet} />
