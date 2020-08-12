8acf97df0416f382b8ea2acadf73a41b2de2feb4
Halit Ak <ak.halit@outlook.com>
2020-08-12 18:35:07 +0300

    search-fill iconu eklendi

---
+++ components/icons/SearchFill.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+function SvgSearchFill(props) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" {...props}>
+      <path
+        d="M22.06 19.94l-3.73-3.73A8.94 8.94 0 0020 11a9 9 0 10-9 9 8.95 8.95 0 005.21-1.67l3.73 3.73c.292.294.676.44 1.06.44s.768-.146 1.06-.44a1.498 1.498 0 000-2.12zM11 17c-3.308 0-6-2.692-6-6s2.692-6 6-6 6 2.692 6 6-2.692 6-6 6z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
+
+export default SvgSearchFill
