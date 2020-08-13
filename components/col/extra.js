fa027236fc0eaa86fbd7baa651a4a7f0933c8d64
Halit Ak <ak.halit@outlook.com>
2020-08-13 11:42:24 +0300

    extra alanı için tasarlanan componentleri yerleştirildi ve extra alanı tasarı için margin değerleri girildi

--- components/col/extra.js
+++ components/col/extra.js
@@ -2,9 +2,36 @@ import React from 'react'
 import cn from 'classnames'
 
 import styles from './extra.module.css'
+import SearchBox from '../search-box'
+import ExtraBox from '../extra-box'
+import TrendsItem from '../trends-item'
+import FollowItem from '../follow-item'
 
 function Sidebar({ children }) {
-  return <div className={cn(styles.extra)}>{children}</div>
+  return (
+    <div className={cn(styles.extra)}>
+      <SearchBox />
+      <ExtraBox title="Turkey trends" options>
+        <TrendsItem
+          order="1"
+          category="Politics"
+          tag="#bilimkurulu"
+          count="2,679"
+        />
+        <TrendsItem
+          order="2"
+          category="News"
+          tag="#TamBağımsızEkonomi"
+          count="22.6K"
+        />
+      </ExtraBox>
+      <ExtraBox title="Who to follow">
+        <FollowItem name="Adem İlter" slug="ademilter" />
+        <FollowItem name="Adem İlter" slug="ademilter" />
+        <FollowItem name="Adem İlter" slug="ademilter" />
+      </ExtraBox>
+    </div>
+  )
 }
 
 export default Sidebar
