2cf2561cb65e4af6dbedc67b7dfeebef7589af37
alihany <alihanyiilmaz@gmail.com>
2020-09-04 22:03:17 +0300

    oluşturulan componentler her sayfaya yerleştirildi

--- pages/notifications.js
+++ pages/notifications.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Layout from '../components/layout'
 
 function NotificationsPage() {
-  return <Layout>Notifications</Layout>
+  return <Layout notification></Layout>
 }
 
 export default NotificationsPage
